603ca58120846129d60a312eb11c91382ddd7064
kpowojski <krystian.powojski@hotmail.com>
2015-05-25 00:12:08 +0200

    Commit after small changes

--- ProjektLAN/Form1.Designer.cs
+++ ProjektLAN/Form1.Designer.cs
@@ -38,11 +38,16 @@
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(15, 34);
+            this.button1.Enabled = false;
+            this.button1.Location = new System.Drawing.Point(15, 12);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(134, 44);
             this.button1.TabIndex = 0;
@@ -53,7 +58,7 @@
             // button2
             // 
             this.button2.Enabled = false;
-            this.button2.Location = new System.Drawing.Point(15, 84);
+            this.button2.Location = new System.Drawing.Point(15, 77);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(134, 45);
             this.button2.TabIndex = 1;
@@ -94,7 +99,8 @@
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(388, 31);
+            this.textBox1.Enabled = false;
+            this.textBox1.Location = new System.Drawing.Point(388, 47);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(339, 20);
             this.textBox1.TabIndex = 3;
@@ -102,7 +108,7 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(176, 34);
+            this.label1.Location = new System.Drawing.Point(176, 54);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(146, 13);
             this.label1.TabIndex = 4;
@@ -111,7 +117,7 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(176, 65);
+            this.label2.Location = new System.Drawing.Point(176, 84);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(206, 13);
             this.label2.TabIndex = 5;
@@ -119,16 +125,67 @@
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(388, 64);
+            this.textBox2.Enabled = false;
+            this.textBox2.Location = new System.Drawing.Point(388, 77);
             this.textBox2.Name = "textBox2";
             this.textBox2.Size = new System.Drawing.Size(338, 20);
             this.textBox2.TabIndex = 6;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(179, 118);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 7;
+            this.button3.Text = "Ukryj";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(180, 12);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(111, 17);
+            this.radioButton1.TabIndex = 9;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "400 Bad Request ";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            this.radioButton1.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(297, 12);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(142, 17);
+            this.radioButton2.TabIndex = 10;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "Block selected webpage";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            this.radioButton2.CheckedChanged += new System.EventHandler(this.radioButton2_CheckedChanged);
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Location = new System.Drawing.Point(446, 13);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(109, 17);
+            this.radioButton3.TabIndex = 11;
+            this.radioButton3.TabStop = true;
+            this.radioButton3.Text = "401 Unauthorized";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            this.radioButton3.CheckedChanged += new System.EventHandler(this.radioButton3_CheckedChanged);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(755, 448);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.radioButton3);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.button3);
             this.Controls.Add(this.textBox2);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
@@ -156,6 +213,10 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton3;
     }
 }
 
--- ProjektLAN/Form1.cs
+++ ProjektLAN/Form1.cs
@@ -10,45 +10,120 @@ using System.Net;
 using Org.BouncyCastle.X509;
 using System.Security.Cryptography.X509Certificates;
 using System.Net.Security;
+using Fiddler;
 
 namespace ProjektLAN
 {
     public partial class Form1 : Form
     {
-        private Capture capture;
+        private BlockPage blockpage;
+        private BlockNet blockNet;
+        private Unauthorized unauth;
         private Logs logs;
         public Form1()
         {
             
             InitializeComponent();
             logs = new Logs(this.listView1);
-            
+
             
         }
 
         private void Form1_FormClosing_1(object sender, FormClosingEventArgs e)
         {
-            Fiddler.FiddlerApplication.Shutdown();
+            FiddlerApplication.Shutdown();
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
-            this.capture = new Capture(this.logs, this.textBox1.Text, this.textBox2.Text);
+            if (radioButton1.Checked == true)
+            {
+                this.blockNet = new BlockNet(this.logs);
+                this.blockNet.start();
+            }
+            else if (radioButton2.Checked == true)
+            {
+                this.blockpage = new BlockPage(this.logs, this.textBox1.Text, this.textBox2.Text);
+                this.blockpage.start();
+                textBoxEnabling(false);
+            }
+            else if (radioButton3.Checked == true)
+            {
+                this.unauth = new Unauthorized(this.logs);
+                this.unauth.start();
+            }
+            else {
+                this.logs.addLog("Select radio button and try again");
+            }
+            
 
-            this.capture.start();
+            
             this.button1.Enabled = false;
             this.button2.Enabled = true;
+            
+            radioButtonsEnabling(false);
         }
 
         private void button2_Click(object sender, EventArgs e)
         {
-            this.capture.stop();
+            if (radioButton1.Checked == true)
+            {
+                this.blockNet.stop();
+                
+            }
+            else if (radioButton2.Checked == true)
+            {
+                this.blockpage.stop();
+                
+            }
+            else
+            {
+                this.unauth.stop();
+            }
+
+            radioButtonsEnabling(true);
             this.button2.Enabled = false;
+            this.button1.Enabled = true;
+            
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+            textBoxEnabling(false);
+            this.button1.Enabled = true;
+
+        }
+
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+            textBoxEnabling(true);
             this.button1.Enabled = true;
         }
 
-        
 
-       
+
+        private void textBoxEnabling(bool var)
+        {
+            this.textBox1.Enabled = var;
+            this.textBox2.Enabled = var;
+        }
+
+        private void radioButton3_CheckedChanged(object sender, EventArgs e)
+        {
+            textBoxEnabling(false);
+            this.button1.Enabled = true;
+        }
+
+        private void radioButtonsEnabling(bool var)
+        {
+            this.radioButton1.Enabled = var;
+            this.radioButton2.Enabled = var;
+            this.radioButton3.Enabled = var;
+        }
     }
 }
